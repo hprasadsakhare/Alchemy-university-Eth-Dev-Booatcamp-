bb4234b99cd7ec45241b94fcc7a0c1de8eeda41d
hprasadsakhare <119033333+hprasadsakhare@users.noreply.github.com>
2023-05-02 11:49:30 +0530

    Add files via upload

---
+++ blockchain cryptography/proof of work'/2-mineblock.js
@@ -0,0 +1,26 @@
+const SHA256 = require("crypto-js/sha256");
+const TARGET_DIFFICULTY =
+    BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+    
+    mempool.push(transaction);
+}
+
+function mine() {
+    const blockHeight = blocks.length;
+    blocks.push({ id: blockHeight });
+}
+
+module.exports = {
+    TARGET_DIFFICULTY,
+    MAX_TRANSACTIONS,
+    addTransaction,
+    mine,
+    blocks,
+    mempool,
+};
